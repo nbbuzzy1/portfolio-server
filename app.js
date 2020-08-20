c294f465afb07baec4b0606c44dc4a43a6cc8ceb
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-08-20 19:40:14 -0400

    Fix production url for front end.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 app.post('/contact-me', (req, res, next) => {
 	const { name, userEmail, message } = req.body;
-
+	
 	const client = new SocketLabsClient(parseInt(process.env.SGSERVERID), process.env.SGINJECTIONAPIKEY);
 
 	const email = {
